c3b6666ed467c1de23bd75620c1cf3ecab997343
Gazi Ataol Coşkun <gaziataolcoskun@gmail.com>
2025-11-19 16:15:01 +0100

    Add PositionAndColor class with encoding methods

---
+++ PositionAndColor.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+public
+    class PositionAndColor {
+
+    public static int encode(int x, int y, Color color){
+        return 0;
+    }
+
+    public static int[] decode(int value) {
+        return new int[]{};
+    }
+
+    public static int colorToByte(Color color){
+        return 0;
+    }
+
+    public static Color byteToColor(int value) {
+        return new Color(0,0,0);
+    }
+}
